afe7ebca9d1287da3267ca04eaf20183119202d8
Iori Pimentel <ioripimentel64@gmail.com>
2023-11-24 00:54:49 +0800

    Add zsh plugins

---
+++ zsh/antidote-plugins.zsh
@@ -0,0 +1,15 @@
+fpath+=( /data/data/com.termux/files/home/.cache/antidote/romkatv/powerlevel10k )
+source /data/data/com.termux/files/home/.cache/antidote/romkatv/powerlevel10k/powerlevel10k.zsh-theme
+source /data/data/com.termux/files/home/.cache/antidote/romkatv/powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /data/data/com.termux/files/home/.cache/antidote/agkozak/zsh-z )
+source /data/data/com.termux/files/home/.cache/antidote/agkozak/zsh-z/zsh-z.plugin.zsh
+fpath+=( /data/data/com.termux/files/home/.cache/antidote/hlissner/zsh-autopair )
+source /data/data/com.termux/files/home/.cache/antidote/hlissner/zsh-autopair/zsh-autopair.plugin.zsh
+fpath+=( /data/data/com.termux/files/home/.cache/antidote/jimhester/per-directory-history )
+source /data/data/com.termux/files/home/.cache/antidote/jimhester/per-directory-history/per-directory-history.plugin.zsh
+fpath+=( /data/data/com.termux/files/home/.cache/antidote/Aloxaf/fzf-tab )
+source /data/data/com.termux/files/home/.cache/antidote/Aloxaf/fzf-tab/fzf-tab.plugin.zsh
+fpath+=( /data/data/com.termux/files/home/.cache/antidote/zsh-users/zsh-autosuggestions )
+source /data/data/com.termux/files/home/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /data/data/com.termux/files/home/.cache/antidote/zsh-users/zsh-syntax-highlighting )
+source /data/data/com.termux/files/home/.cache/antidote/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
