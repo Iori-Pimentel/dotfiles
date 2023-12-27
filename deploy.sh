8ace1ad2d361e857f31da4af8bbe072a8d2b5760
Iori Pimentel <ioripimentel64@gmail.com>
2023-12-27 16:44:40 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # Zsh setup script for Termux-Monet
 # https://github.com/HardcodedCat/termux-monet
 
-set -e
+set -o errexit
 
 [[ -d ~/storage ]] || termux-setup-storage
 
@@ -11,7 +11,7 @@ ln -sfT "${PREFIX}/etc/termux/"{mirrors/asia,chosen_mirrors}
 
 pkg upgrade -y
 packages=(
-  zsh git # dependencies
+  zsh git
   manpages tealdeer
   eza bat
   fd ripgrep
@@ -22,7 +22,7 @@ packages=(
   clang # c compiler for nvim-treesitter 
   lua-language-server # mason-lspconfig replacement for lua
   python # nvim-jdtls dependencies
-) && pkg install -y ${packages[@]}
+) && pkg install -y "${packages[@]}"
 
 # Default XDG paths
 XDG_CACHE_HOME="${HOME}/.cache"
@@ -30,34 +30,38 @@ XDG_CONFIG_HOME="${HOME}/.config"
 XDG_DATA_HOME="${HOME}/.local/share"
 XDG_STATE_HOME="${HOME}/.local/state"
 
-# Required Directories
-mkdir -p "${XDG_DATA_HOME}/zsh" # Needed for history file
-mkdir -p "${XDG_CONFIG_HOME}/git"
-mkdir -p "${HOME}/.local/bin"
-
+args=(
+	--parents
+	"${XDG_DATA_HOME}/zsh" # for zsh HISTFILE
+	"${XDG_CONFIG_HOME}/git"
+	"${HOME}/.local/bin"
+) && mkdir "${args[@]}" && unset args
 
 DOTFILES="${HOME}/.local/dotfiles"
-
 git clone https://github.com/Iori-Pimentel/dotfiles.git  "${DOTFILES}"
 git clone https://github.com/mattmc3/antidote.git        "${DOTFILES}/zsh/.antidote" --depth=1
 
-ln -sfT "${DOTFILES}/zsh/.zshenv"        ~/.zshenv
-ln -sfT "${DOTFILES}/configs/gitconfig"  "${XDG_CONFIG_HOME}/git/config"
-ln -sfT "${DOTFILES}/nvim"               "${XDG_CONFIG_HOME}/a-nvim"
+args=(--symbolic --force --no-target-directory)
+ln "${args[@]}" "${DOTFILES}/zsh/.zshenv"        "${HOME}/.zshenv"
+ln "${args[@]}" "${DOTFILES}/configs/gitconfig"  "${XDG_CONFIG_HOME}/git/config"
+ln "${args[@]}" "${DOTFILES}/nvim"               "${XDG_CONFIG_HOME}/a-nvim"
+unset args
 
 COLORS='https://github.com/adi1090x/termux-style/raw/master/colors/gruvbox-dark.properties'
 FONT='https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf'
 FONT_ITALIC='https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts%2FJetBrainsMono%2FNoLigatures%2FItalic%2FJetBrainsMonoNLNerdFont-Italic.ttf'
-
-curl -fsSL $COLORS $FONT $FONT_ITALIC -o ~/.termux/colors.properties -o ~/.termux/font.ttf -o ~/.termux/font-italic.ttf
-
 ANDROFETCH='https://github.com/laraib07/androfetch/raw/main/androfetch'
-BIN_DIR="${HOME}/.local/bin"
 
-mv-bin() { chmod u+x "$@" && mv "$@" "$BIN_DIR"; }
-curl -fsSL -O $ANDROFETCH && mv-bin androfetch
-
-# scripts
+args=(
+	--fail
+	--location # follow 3xx response code
+	--silent --show-error # disables progress meter but still show error messages
+	"$COLORS"      -o ~/.termux/colors.properties
+	"$FONT"        -o ~/.termux/font.ttf
+	"$FONT_ITALIC" -o ~/.termux/font-italic.ttf
+	"$ANDROFETCH"  -o ~/.local/bin/androfetch
+) && curl "${args[@]}" && unset args
+chmod u+x ~/.local/bin/androfetch
 
 tldr --update
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
