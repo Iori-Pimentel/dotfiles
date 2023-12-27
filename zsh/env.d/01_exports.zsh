5b72d6de202b9848b782834c050bccd55bdb7959
Iori Pimentel <ioripimentel64@gmail.com>
2023-12-27 16:46:55 +0800

    Remove checks for existance of XDG dirs

--- zsh/env.d/01_exports.zsh
+++ zsh/env.d/01_exports.zsh
@@ -5,10 +5,10 @@ export PAGER=less
 export LESS="--RAW-CONTROL-CHARS --jump-target .7 --ignore-case --hilite-unread --LONG-PROMPT --window=-4 --tabs=4"
 
 # XDG basedir spec compliance 
-[[ -v XDG_CONFIG_HOME ]] || export XDG_CONFIG_HOME"=${HOME}/.config"
-[[ -v XDG_CACHE_HOME  ]] || export XDG_CACHE_HOME="${HOME}/.cache"
-[[ -v XDG_DATA_HOME   ]] || export XDG_DATA_HOME="${HOME}/.local/share"
-[[ -v XDG_STATE_HOME  ]] || export XDG_STATE_HOME="${HOME}/.local/state"
+export XDG_CONFIG_HOME"=${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
 
 # best effort to make tools compliant to XDG basedir spec
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
