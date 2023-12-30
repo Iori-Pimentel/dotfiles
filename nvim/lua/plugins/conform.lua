4dfbdb5d0083ee69ef9c3a5f6e52a761bbb7cff5
Iori Pimentel <ioripimentel64@gmail.com>
2023-12-30 17:30:33 +0800

    Remove lsp on nvim

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,14 +1,12 @@
 return {
 	"stevearc/conform.nvim",
-	config = function() -- lsp also has formatter enabled
+	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 			},
 			format_on_save = {
-				-- These options will be passed to conform.format()
 				timeout_ms = 500,
-				lsp_fallback = true,
 			},
 		})
 	end,
