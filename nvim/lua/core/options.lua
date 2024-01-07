1f7bfa0512aee1b54ed959bb37bff9b3d9797dbe
Iori Pimentel <ioripimentel64@gmail.com>
2024-01-08 00:26:25 +0800

    Update nvim config

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -10,24 +10,33 @@ opt.cursorline = true
 opt.ignorecase = true
 opt.smartcase = true
 opt.background = "dark"
-opt.laststatus = 3 -- enables global statusline
+opt.laststatus = 3 -- enables global statusline for split windows
 opt.signcolumn = "yes:1" -- left side padding
 opt.wildignorecase = true -- for completion of file names in command mode
 opt.cmdwinheight = 5
 opt.virtualedit = "block"
 opt.list = true
+opt.numberwidth = 1 -- minimize width of line numbers
+opt.colorcolumn = { 79, 80 }
 opt.listchars = {
-	trail = "~",
-	extends = "~",
-	precedes = "~",
-	lead = "~",
 	tab = "| ",
-	-- |hl-NonText| highlighting will be used for "eol", "extends" and "precedes".
-	-- |hl-Whitespace| for "nbsp", "space", "tab", "multispace", "lead" and "trail".
+	precedes = "~", -- overflow symbol when wrap = false
+	extends = "~", -- overflow symbol when wrap = false
+	trail = ".", -- symbol for trailing spaces
 }
+
+-- overflow symbol when wrap = true
+opt.fillchars = { lastline = "~" }
+-- Disable mouse in insert mode to prevent misclicks when typing
+opt.mouse = "nv"
+
 vim.cmd([[colorscheme habamax]])
 -- Whitespace highlight links to NonText on habamax colorscheme
-vim.cmd([[highlight NonText cterm=bold ctermfg=003]])
+vim.cmd([[
+	highlight NonText cterm=bold ctermfg=003
+	highlight StatusLine ctermbg=003
+	highlight clear DiffChange ctermfg=145
+]])
 
 if vim.fn.executable("rg") == 1 then
 	opt.grepprg = "rg --vimgrep"
@@ -37,7 +46,6 @@ else
 	opt.grepprg = "git grep -n"
 end
 
--- tabs & indentation
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.softtabstop = 2
--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -2,7 +2,14 @@ return {
 	"numToStr/Comment.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		require("Comment").setup()
+		local comment = require("Comment")
+		comment.setup({
+			pre_hook = function()
+				-- Prevents adding comment when pressing o/O in normal mode
+				-- FIXME: only triggers when plugin triggers
+				vim.opt.formatoptions:remove("o")
+			end,
+		})
 		local ft = require("Comment.ft")
 		ft.set("text", "# %s")
 	end,
