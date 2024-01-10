8e83b1668aefca77bddb0c69f40f5ec3783b18fc
Iori Pimentel <ioripimentel64@gmail.com>
2024-01-11 00:39:21 +0800

    Add mapping to comment.nvim

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -1,12 +1,14 @@
 local map = vim.keymap.set
 -- opt.clipboard:append("unnamedplus") -- use system clipboard on all actions
 map({ "n", "v" }, "y", '"+y') -- use system clipboard on yank only
+map("n", "<leader>n", "<CMD>set number!<CR>")
+
+-- bufferline.nvim
 map("n", "H", "<CMD>BufferLineCyclePrev<CR>")
 map("n", "L", "<CMD>BufferLineCycleNext<CR>")
-map("n", "<leader>n", "<CMD>set number!<CR>")
-map("n", "<ESC>", "<CMD>noh<CR>")
 
 -- Comment.nvim
+map("v", "<leader>/", "gc", { remap = true })
 map("n", "<leader>/", "gcc", { remap = true })
 map("n", "<leader>a", "gcA", { remap = true })
 map("n", "<leader>o", "gco", { remap = true })
