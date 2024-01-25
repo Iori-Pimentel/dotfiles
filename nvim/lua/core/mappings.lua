4b601929eaee45aaf789f8c9dc545132eaef6531
Iori Pimentel <ioripimentel64@gmail.com>
2024-01-25 12:56:09 +0800

    nvim: add keyword replacement under cursor mapping

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -2,6 +2,7 @@ local map = vim.keymap.set
 -- opt.clipboard:append("unnamedplus") -- use system clipboard on all actions
 map({ "n", "v" }, "y", '"+y') -- use system clipboard on yank only
 map("n", "<leader>n", "<CMD>set number!<CR>")
+map({ "n", "v" }, "<leader>*", "*Ncgn", { remap = true }) -- replace keyword under cursor
 
 -- bufferline.nvim
 map("n", "H", "<CMD>BufferLineCyclePrev<CR>")
