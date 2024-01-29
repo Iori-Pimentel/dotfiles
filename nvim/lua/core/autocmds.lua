52c4cf29c5c2e77f5b13ae2bae26355f6fe7d1e7
Iori Pimentel <ioripimentel64@gmail.com>
2024-01-29 09:37:38 +0800

    nvim: add augroup Default

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,6 +1,9 @@
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local default = augroup("Default", {})
 
 autocmd("TextYankPost", {
+	group = default,
 	callback = function()
 		-- highlights yank
 		vim.highlight.on_yank({ timeout = 250, on_macro = true })
@@ -15,6 +18,7 @@ autocmd("TextYankPost", {
 
 -- display tab and trailing space when on first column
 autocmd({ "CursorMoved", "CursorMovedI" }, {
+	group = default,
 	callback = function()
 		local col = vim.fn.getpos(".")[3]
 		vim.wo.list = col == 1 and vim.bo.modifiable
@@ -23,6 +27,7 @@ autocmd({ "CursorMoved", "CursorMovedI" }, {
 
 -- ftplugin unwanted behaviour fix
 autocmd("FileType", {
+	group = default,
 	callback = function()
 		-- disables automatic comment after hitting o in normal mode
 		vim.opt.formatoptions:remove("o")
@@ -31,6 +36,7 @@ autocmd("FileType", {
 
 -- bufferline.nvim add :help buffers
 autocmd("BufWinEnter", {
+	group = default,
 	callback = function()
 		if vim.bo.filetype == "help" then
 			vim.cmd.only()
