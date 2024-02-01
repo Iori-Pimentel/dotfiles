34f3e9a6a168f7cb00700a5a1b8a7816de4fabb8
Iori Pimentel <ioripimentel64@gmail.com>
2024-02-02 00:34:23 +0800

    zsh: moved p10k config to own file

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -21,6 +21,3 @@ for conffile in "${ZDOTDIR}"/rc.d/*.zsh; do
     source "${conffile}" 
 done 
 unset conffile
-
-# To customize prompt, run `p10k configure` or edit ~/.local/dotfiles/zsh/.p10k.zsh.
-[[ ! -f ~/.local/dotfiles/zsh/.p10k.zsh ]] || source ~/.local/dotfiles/zsh/.p10k.zsh
---
+++ zsh/rc.d/08_p10k_config.zsh
@@ -0,0 +1,18 @@
+# Custom configuration for p10k prompt.
+# Also see: 07_p10k.zsh (Generated using `p10k configure`)
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+  os_icon dir per_directory_history vcs status
+	newline prompt_char
+)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
+
+POWERLEVEL9K_DIR_MAX_LENGTH=30
+# allows shortening current directory
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=0
+
+POWERLEVEL9K_STATUS_OK=false # 0
+POWERLEVEL9K_STATUS_OK_PIPE=true # 0 | 1
+POWERLEVEL9K_STATUS_ERROR=true # 1
+# Turns off status icons
+typeset -m 'POWERLEVEL9K_STATUS_*_VISUAL_IDENTIFIER_EXPANSION'=
