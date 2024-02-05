5d4d91566dafe7ac5c618314e338f09c77e31839
Iori Pimentel <ioripimentel64@gmail.com>
2024-02-06 04:31:54 +0800

    zsh: clean and update misc.zsh

--- zsh/rc.d/02_misc.zsh
+++ zsh/rc.d/02_misc.zsh
@@ -1,34 +1,23 @@
-# History ----------
 mkdir -p "${XDG_DATA_HOME}/zsh"
 HISTFILE="${XDG_DATA_HOME}/zsh/history"
 HISTSIZE=10000000
 SAVEHIST=10000000
 
-# cd into last exited directory
-autoload session-cd && {
-	session-cd load
-	TRAPEXIT() { session-cd save; }
-}
-
-# Bar Cursor ----------
-# <Docs> man zshzle | less +/Special.Widgets +/zle-line </Docs>
-barcursor() { echo -ne '\e[5 q'; }
-autoload redraw-prompt
-zle-line-init() { barcursor }
-zle-line-pre-redraw() { barcursor } # fix for edit-command-line widget
-# zle-line-pre-redraw() { barcursor; _zsh_highlight; -fast-highlight-init } # fix for edit-command-line widget
+# <Docs> man zshzle | less +/Special.Widgets </Docs>
+barcursor() { printf '\e[5 q'; }
 zle -N zle-line-pre-redraw
 zle -N zle-line-init
+zle-line-init() { barcursor }
+zle-line-pre-redraw() { barcursor }
 
-# Hash Directories ----------
-hash -d prefix="$PREFIX"
-hash -d downloads="${HOME}/storage/downloads"
+# <Docs> man zshmisc | less +/SPECIAL.FUNCTIONS </Docs>
+autoload session-cd
+session-cd load
+chpwd() { session-cd save; }
 
-# Completions ----------
-autoload -Uz compinit && compinit
+hash -d PREFIX="$PREFIX"
 
-# Fzf Keybindings ----------
 source "${PREFIX}/share/fzf/key-bindings.zsh"
 
-# Force path arrays to have unique values only
+# Removes duplicate entries
 typeset -U path cdpath fpath manpath
