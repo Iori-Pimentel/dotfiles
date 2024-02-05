f9a1a7263ca5897d5c48d6d96d9dd3d233a4dc3f
Iori Pimentel <ioripimentel64@gmail.com>
2024-02-06 04:16:05 +0800

    zsh: clean plugins.zsh

--- zsh/rc.d/03_plugins.zsh
+++ zsh/rc.d/03_plugins.zsh
@@ -1,53 +1,37 @@
-# ==== z ====
-
-ZSHZ_DATA="${XDG_CACHE_HOME}/zsh/z"
-ZSHZ_UNCOMMON=1 # match to uncommon prefix
-ZSHZ_CASE=smart # ignore case when lowercase, match case with uppercase
-
-
-# ==== fzf-tab ====
-# TODO: preview
-#zstyle ':fzf-tab:complete:eza:*'
-#zstyle ':fzf-tab::complete:*:directories' fzf-preview 'eza -1 --color=always $realpath'
-
-# Nvim completion on recently opened files
-
-zstyle ':fzf-tab:*' switch-group ':' '"'
-zstyle ':fzf-tab:*' continuous-trigger 'tab'
-zstyle ':fzf-tab:*' prefix ''
-
-## Completion tweaks
-zstyle ':completion:*:default'      list-colors       "${(s.:.)LS_COLORS}"
-zstyle ':completion:*'              list-dirs-first   true
-zstyle ':completion:*'              verbose           true
-zstyle ':completion:*:all-files'    matcher           'r:|=** l:|=** m:{a-z\-}={A-Z\_}' #TODO: Understand
-# zstyle ':completion::complete:*'    use-cache         true
-# zstyle ':completion::complete:*'    cache-path        "${XDG_CACHE_HOME}/zsh/compcache"
-zstyle ':completion:*:descriptions' format            [%d]
-zstyle ':completion:*:manuals.*'    insert-sections   true
-zstyle ':completion:*:manuals'      separate-sections true
-# zstyle ':completion:*:man:*'      menu yes select
-# zstyle ':fzf-tab:complete:man:*' show-group none
-
-# environment variable preview
-# fix nowrap
-# zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' \
-#   fzf-preview 'echo ${(P)word}'
-
-# ==== auto-suggestions ====
-
-ZSH_AUTOSUGGEST_MANUAL_REBIND=true
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(accept-line bracketed-paste fzf-tab-complete edit-command-line)
-ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(forward-word forward-word-path)
-
-bindkey -e
-
-# ==== per-directory-history ====
-
 HISTORY_BASE="${XDG_DATA_HOME}/zsh/directory-history"
 
-# ==== antidote ====
+ZSHZ_DATA="${XDG_CACHE_HOME}/zsh/z"
+ZSHZ_NO_RESOLVE_SYMLINKS=1
+
+# hides unwanted prefix
+zstyle ':fzf-tab:*' prefix ''
+# enables colors for fzf-tab
+zstyle ':completion:*:descriptions' format [%d]
+# removes header display
+zstyle ':fzf-tab:*' show-group none
+# applies color even for a single group
+zstyle ':fzf-tab:*' single-group color
+
+zstyle ':completion:*'            list-dirs-first  true
+zstyle ':completion:*:cd:*'       complete-options yes
+zstyle ':completion:*:parameters' ignored-patterns '_*'
+autoload compinit && compinit
+
+# disables rebind on every precmd
+ZSH_AUTOSUGGEST_MANUAL_REBIND=true
+# default strategy: history
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(
+	accept-line
+	bracketed-paste
+	fzf-tab-complete
+	edit-command-line
+	quoted-insert
+)
+ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(
+	forward-word
+	forward-word-path
+)
 
 zstyle ':antidote:bundle' use-friendly-names 'yes'
 source "${ZDOTDIR}/.antidote/antidote.zsh"
