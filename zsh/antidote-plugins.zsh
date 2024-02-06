b6e9f65497d180b8a24db3d19f70119934b9fe85
Iori Pimentel <ioripimentel64@gmail.com>
2024-02-06 09:13:31 +0800

    zsh: remove zsh autopair

--- zsh/antidote-plugins.zsh
+++ zsh/antidote-plugins.zsh
@@ -3,8 +3,6 @@ source /data/data/com.termux/files/home/.cache/antidote/romkatv/powerlevel10k/po
 source /data/data/com.termux/files/home/.cache/antidote/romkatv/powerlevel10k/powerlevel9k.zsh-theme
 fpath+=( /data/data/com.termux/files/home/.cache/antidote/agkozak/zsh-z )
 source /data/data/com.termux/files/home/.cache/antidote/agkozak/zsh-z/zsh-z.plugin.zsh
-fpath+=( /data/data/com.termux/files/home/.cache/antidote/hlissner/zsh-autopair )
-source /data/data/com.termux/files/home/.cache/antidote/hlissner/zsh-autopair/zsh-autopair.plugin.zsh
 fpath+=( /data/data/com.termux/files/home/.cache/antidote/jimhester/per-directory-history )
 source /data/data/com.termux/files/home/.cache/antidote/jimhester/per-directory-history/per-directory-history.plugin.zsh
 fpath+=( /data/data/com.termux/files/home/.cache/antidote/Aloxaf/fzf-tab )
