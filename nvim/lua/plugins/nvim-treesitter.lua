e6035a31f7ba740f41bc738c55b76b075fbafc11
Iori Pimentel <ioripimentel64@gmail.com>
2024-02-07 01:05:01 +0800

    nvim: clean

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,14 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
-		build = ":TSUpdate",
-		config = function()
-			local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-			treesitter.setup({
-				highlight = { enable = true },
-				indent = { enable = true },
-				auto_install = true,
-			})
-		end,
-	},
+		treesitter.setup({
+			highlight = { enable = true },
+			indent = { enable = true },
+			auto_install = true,
+		})
+	end,
 }
