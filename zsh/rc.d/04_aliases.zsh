3a1a9aaba4894565af85fe493b1ea6402ad81f93
Iori Pimentel <ioripimentel64@gmail.com>
2024-02-07 19:50:41 +0800

    zsh: ignore q and qq in history

--- zsh/rc.d/04_aliases.zsh
+++ zsh/rc.d/04_aliases.zsh
@@ -2,8 +2,9 @@ alias eza='eza --all --no-user --icons=always --color=always --git --group-direc
 alias ll='eza --long'
 alias lt='eza --tree --git-ignore --level 2'
 
-alias q='exit'
-alias qq='clear && session-cd save && exec zsh'
+alias qq=' clear && session-cd save && exec zsh'
+alias q=' exit'
+
 alias z='zshz /data/data/com.termux/files 2>&1'
 alias n='nvim'
 
