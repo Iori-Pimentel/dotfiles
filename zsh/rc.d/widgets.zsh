ae4ebe4f46a2a86666fec6ba5825078b7a39c739
Iori Pimentel <ioripimentel64@gmail.com>
2024-03-12 15:54:22 +0800

    zsh: fzf-files display fix for newlines

--- zsh/rc.d/widgets.zsh
+++ zsh/rc.d/widgets.zsh
@@ -95,7 +95,11 @@ fzf-files() {
 		--border-label-pos=3
 		# Display on border if selection has non-printable character
 		--bind='focus:transform-border-label(
-			[[ {} =~ [^[:print:]] ]] && cat -v <<< "{}"
+			[[ {} =~ [^[:print:]] ]] && cat -v <<< {} |
+				# Remove trailing newline
+				head --bytes=-1 |
+				# Translate newline as supplement to `cat -v`
+				tr "\n" "$"
 		)'
 	)
 
