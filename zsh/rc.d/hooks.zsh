5dbc732836e4404d8803844fc65ad1eb10392812
Iori Pimentel <ioripimentel64@gmail.com>
2024-06-10 18:16:01 +0800

    zsh: Update hooks.zsh and Add fpath/set-title

--- zsh/rc.d/hooks.zsh
+++ zsh/rc.d/hooks.zsh
@@ -1,28 +1,35 @@
+# <Docs> man zshcontrib | less +/add-zle-hook-widget </Docs>
 # <Docs> man zshzle | less +/Special.Widgets </Docs>
-barcursor() { printf '\e[5 q'; }
-zle -N zle-line-init
-zle -N zle-line-pre-redraw
-zle-line-init() { barcursor }
-zle-line-pre-redraw() { barcursor }
+autoload add-zle-hook-widget
 
-session-cd load
+zle -N barcursor
+add-zle-hook-widget line-init       barcursor
+add-zle-hook-widget line-pre-redraw barcursor
+barcursor() {
+	printf '\e[5 q' > $TTY
+}
 
+# <Docs> man zshcontrib | less +/add-zsh-hook </Docs>
 # <Docs> man zshmisc | less +/SPECIAL.FUNCTIONS </Docs>
-chpwd() {
+autoload add-zsh-hook
+
+add-zsh-hook chpwd session-save
+session-save() {
 	session-cd save
 }
 
+# Called only once per session
+session-cd load
+
 # https://github.com/jimhester/per-directory-history/issues/31
 # Not using preexec() since $1 is empty caused by per-directory-history
-zshaddhistory() {
-	BUFFER=$1
-	# Replace : and non-printable characters with space
-	# TODO: Find out why : breaks the title
-	BUFFER=${BUFFER//[!:[:print:]]/ }
-	printf '\e]2;%s\a' ${BUFFER} > $TTY
+add-zsh-hook zshaddhistory command-title
+command-title() {
+	BUFFER="$1"
+	set-title "$BUFFER"
 }
 
-precmd() {
-	printf '\e]2;%s\a' "${(D)PWD}" > $TTY
+add-zsh-hook precmd directory-title
+directory-title() {
+	set-title "${(D)PWD}"
 }
-
