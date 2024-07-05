57aa877dd4e96bf575cffe636c3f82ba6d9e8576
Iori Pimentel <ioripimentel64@gmail.com>
2024-07-05 20:17:02 +0800

    zsh: Simplify directory-history hook functions

--- zsh/rc.d/directory-history.zsh
+++ zsh/rc.d/directory-history.zsh
@@ -1,26 +1,31 @@
 add-zsh-hook zshaddhistory mark-history-line
 mark-history-line() {
-	local PWD_HISTFILE=$HISTORY_BASE$PWD/history
+	[[ ${PWD:t} == $HIST_NAME ]] && return
 
-	mkdir --parent ${PWD_HISTFILE:h}
-
-	cat $CURRENT_HISTFILE > $CURRENT_HISTFILE.temp
+	mkdir --parent ${CURRENT_HISTFILE:h}
 	HISTFILE=$CURRENT_HISTFILE
 
 	setopt INC_APPEND_HISTORY
+	touch $CURRENT_HISTFILE
+	cp --no-target-directory $CURRENT_HISTFILE $CURRENT_HISTFILE.old
+
+	# <Docs> man zshmisc | less +/zshaddhistory +/returns.status </Docs>
+	return 0
 }
 
 add-zsh-hook preexec save-history-line
 save-history-line() {
-	local PWD_HISTFILE=$HISTORY_BASE$PWD/history
-	[[ -s $CURRENT_HISTFILE.temp ]] || return
+	[[ ${PWD:t} == $HIST_NAME ]] && return
 
-	comm -13 $CURRENT_HISTFILE.temp $CURRENT_HISTFILE >> $OTHER_HISTFILE
-
-	rm $CURRENT_HISTFILE.temp
-
-	# We want to preserve HISTFILE
-	HISTFILE=$GLOBAL_HISTFILE
+	if [[ -f $CURRENT_HISTFILE.old && -f $CURRENT_HISTFILE ]]; then
+		local COMM_ARGS=(
+			-1 -3 # output lines unique to CURRENT_HISTFILE
+			$CURRENT_HISTFILE.old
+			$CURRENT_HISTFILE
+		)
+		mkdir --parent ${OTHER_HISTFILE:h}
+		comm "${COMM_ARGS[@]}" >> $OTHER_HISTFILE
+	fi
 }
 
 add-zsh-hook precmd start-history-precmd
@@ -28,6 +33,11 @@ start-history-precmd() {
 	[[ $HISTORY_BASE == '/'* ]] || HISTORY_BASE=${XDG_DATA_HOME}/zsh/directory-history
 	[[ $HIST_START_LOCAL == false ]] || HIST_START_LOCAL=true
 	GLOBAL_HISTFILE=$HISTFILE
+	if [[ $HIST_NAME == */* || $HIST_NAME == '' ]]; then
+	# We make HIST_NAME really long so that it
+	# becomes unlikely to conflict with a directory name
+		HIST_NAME=directory-local-history-file.txt
+	fi
 
 	if [[ $HIST_START_LOCAL == true ]]; then
 		local-history-list
@@ -43,7 +53,7 @@ start-history-precmd() {
 }
 
 local-history-list() {
-	local PWD_HISTFILE=$HISTORY_BASE$PWD/history
+	local PWD_HISTFILE=$HISTORY_BASE$PWD/$HIST_NAME
 
 	fc -P
 	fc -p $PWD_HISTFILE
@@ -56,7 +66,7 @@ local-history-list() {
 }
 
 global-history-list() {
-	local PWD_HISTFILE=$HISTORY_BASE$PWD/history
+	local PWD_HISTFILE=$HISTORY_BASE$PWD/$HIST_NAME
 
 	fc -P
 	fc -p $GLOBAL_HISTFILE
