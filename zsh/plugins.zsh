f22abfbdccca84e0be4ef7720d99e8ef5055c212
Iori Pimentel <ioripimentel64@gmail.com>
2024-08-01 16:23:53 +0800

    zsh: Antidote plugins.zsh change

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,11 +1,11 @@
-fpath+=( /data/data/com.termux/files/home/.cache/antidote/romkatv/powerlevel10k )
-source /data/data/com.termux/files/home/.cache/antidote/romkatv/powerlevel10k/powerlevel10k.zsh-theme
-source /data/data/com.termux/files/home/.cache/antidote/romkatv/powerlevel10k/powerlevel9k.zsh-theme
-fpath+=( /data/data/com.termux/files/home/.cache/antidote/agkozak/zsh-z )
-source /data/data/com.termux/files/home/.cache/antidote/agkozak/zsh-z/zsh-z.plugin.zsh
-fpath+=( /data/data/com.termux/files/home/.cache/antidote/Aloxaf/fzf-tab )
-source /data/data/com.termux/files/home/.cache/antidote/Aloxaf/fzf-tab/fzf-tab.plugin.zsh
-fpath+=( /data/data/com.termux/files/home/.cache/antidote/zsh-users/zsh-autosuggestions )
-source /data/data/com.termux/files/home/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /data/data/com.termux/files/home/.cache/antidote/zsh-users/zsh-syntax-highlighting )
-source /data/data/com.termux/files/home/.cache/antidote/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( $HOME/.cache/antidote/romkatv/powerlevel10k )
+source $HOME/.cache/antidote/romkatv/powerlevel10k/powerlevel10k.zsh-theme
+source $HOME/.cache/antidote/romkatv/powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( $HOME/.cache/antidote/agkozak/zsh-z )
+source $HOME/.cache/antidote/agkozak/zsh-z/zsh-z.plugin.zsh
+fpath+=( $HOME/.cache/antidote/Aloxaf/fzf-tab )
+source $HOME/.cache/antidote/Aloxaf/fzf-tab/fzf-tab.plugin.zsh
+fpath+=( $HOME/.cache/antidote/zsh-users/zsh-autosuggestions )
+source $HOME/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( $HOME/.cache/antidote/zsh-users/zsh-syntax-highlighting )
+source $HOME/.cache/antidote/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
