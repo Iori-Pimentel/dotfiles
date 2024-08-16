3de82da803a83ef79d4e613c7a603c26bc6b19fa
Iori Pimentel <ioripimentel64@gmail.com>
2024-08-16 12:41:41 +0800

    nvim: Disable config change updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,4 +15,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	checker = { enabled = false },
+	change_detection = { enabled = false },
+})
