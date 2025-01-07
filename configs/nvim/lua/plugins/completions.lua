cfe39abdcf41cc9709a64452affa73a0151eadfa
Iori Pimentel <ioripimentel64@gmail.com>
2025-01-07 12:57:14 +0800

    nvim: Add back Luasnip for better snippet navigation

--- configs/nvim/lua/plugins/completions.lua
+++ configs/nvim/lua/plugins/completions.lua
@@ -1,6 +1,11 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = "rafamadriz/friendly-snippets",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		-- !Important! Make sure you're using the latest release of LuaSnip
+		-- `main` does not work at the moment
+		{ "L3MON4D3/LuaSnip", version = "v2.*" },
+	},
 
 	opts = {
 		keymap = {
@@ -18,6 +23,21 @@ return {
 			menu = {
 				max_height = 6,
 			},
+		},
+
+		snippets = {
+			expand = function(snippet)
+				require("luasnip").lsp_expand(snippet)
+			end,
+			active = function(filter)
+				if filter and filter.direction then
+					return require("luasnip").jumpable(filter.direction)
+				end
+				return require("luasnip").in_snippet()
+			end,
+			jump = function(direction)
+				require("luasnip").jump(direction)
+			end,
 		},
 	},
 }
