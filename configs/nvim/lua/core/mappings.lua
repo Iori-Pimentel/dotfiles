9d7153500b82205fb19bb644e7ed4ffe93621253
Iori Pimentel <ioripimentel64@gmail.com>
2025-01-07 12:27:09 +0800

    nvim: Convert visual+select mappings to visual only

--- configs/nvim/lua/core/mappings.lua
+++ configs/nvim/lua/core/mappings.lua
@@ -1,15 +1,15 @@
 local map = vim.keymap.set
-map({ "n", "v" }, " ", "<Nop>")
+map({ "n", "x" }, " ", "<Nop>")
 vim.g.mapleader = " "
 
 map("n", "<leader>n", "<CMD>setlocal number! hlsearch! wrap! <CR>")
 
 -- clipboard and blackhole registers
-map({ "n", "v" }, [[+]], [["+]])
-map({ "n", "v" }, [[_]], [["_]])
+map({ "n", "x" }, [[+]], [["+]])
+map({ "n", "x" }, [[_]], [["_]])
 
-map({ "n", "v" }, "j", "gj")
-map({ "n", "v" }, "k", "gk")
+map({ "n", "x" }, "j", "gj")
+map({ "n", "x" }, "k", "gk")
 
 map("n", "[q", "<CMD>cprev<CR>")
 map("n", "]q", "<CMD>cnext<CR>")
@@ -30,8 +30,8 @@ map("o", [[a`]], [[2i`]])
 
 -- excludes newline on visual selection
 -- keeps column position on visual block
-map("v", "{", [[<CMD>silent! keeppatterns ?\v(\n^$|%^)<CR>]])
-map("v", "}", [[<CMD>silent! keeppatterns /\v(\n^$|%$)<CR>]])
+map("x", "{", [[<CMD>silent! keeppatterns ?\v(\n^$|%^)<CR>]])
+map("x", "}", [[<CMD>silent! keeppatterns /\v(\n^$|%$)<CR>]])
 
 map("n", "\\", "<CMD>ls<Cr>:b ")
 map("n", "H", "<CMD>bprev<CR>")
