9f3e6410d811245548562ac08fea4e5972d779ea
Iori Pimentel <ioripimentel64@gmail.com>
2025-01-08 10:14:20 +0800

    nvim: add treesitter textobjects select and swap

--- configs/nvim/lua/plugins/nvim-treesitter.lua
+++ configs/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,36 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	config = function()
 		local treesitter = require("nvim-treesitter.configs")
 
+		---@diagnostic disable-next-line missing-fields
 		treesitter.setup({
 			highlight = { enable = true },
 			indent = { enable = true },
 			auto_install = true,
+
+			textobjects = {
+				select = {
+					enable = true,
+					keymaps = {
+						["ia"] = "@parameter.inner",
+						["aa"] = "@parameter.outer",
+					},
+				},
+
+				swap = {
+					enable = true,
+					swap_next = {
+						["<S-right>"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<S-left>"] = "@parameter.inner",
+					},
+				},
+			},
 		})
 	end,
 }
