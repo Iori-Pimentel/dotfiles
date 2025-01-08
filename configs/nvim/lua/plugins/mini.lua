e9d23da6017a0b41985601cf859f1cddc914bb2f
Iori Pimentel <ioripimentel64@gmail.com>
2025-01-08 15:19:23 +0800

    nvim: Add mini.surround and mini.move

---
+++ configs/nvim/lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+	{ "echasnovski/mini.surround", opts = {} },
+	{
+		"echasnovski/mini.move",
+		opts = {
+			mappings = {
+				down = "<S-down>",
+				up = "<S-up>",
+				line_down = "<S-down>",
+				line_up = "<S-up>",
+			},
+		},
+	},
+}
