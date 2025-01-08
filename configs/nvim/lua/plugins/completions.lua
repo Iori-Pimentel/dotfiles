9ce705b56635e03ccba41063cb2889c27e3bbb91
Iori Pimentel <ioripimentel64@gmail.com>
2025-01-08 09:47:56 +0800

    nvim: Update blink.nvim and disable highlighting documentation

--- configs/nvim/lua/plugins/completions.lua
+++ configs/nvim/lua/plugins/completions.lua
@@ -29,6 +29,7 @@ return {
 		completion = {
 			documentation = {
 				auto_show = true,
+				treesitter_highlighting = false, -- Disables wrong highlighting
 				window = {
 					min_width = 80,
 				},
@@ -43,18 +44,7 @@ return {
 		},
 
 		snippets = {
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
+			preset = "luasnip",
 		},
 	},
 }
