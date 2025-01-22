672700b299fd5646db60a6ba6ba3876adaf63622
Iori Pimentel <ioripimentel64@gmail.com>
2025-01-22 22:01:06 +0800

    nvim: Add mini plugins

--- configs/nvim/lua/plugins/mini.lua
+++ configs/nvim/lua/plugins/mini.lua
@@ -1,14 +1,43 @@
 return {
-	{ "echasnovski/mini.surround", opts = {} },
 	{
-		"echasnovski/mini.move",
+		"chasnovski/mini.surround",
 		opts = {
 			mappings = {
-				down = "<S-down>",
-				up = "<S-up>",
-				line_down = "<S-down>",
-				line_up = "<S-up>",
+				add = "s",
+				delete = "<BS>",
+				replace = "<C-H>", -- <Ctrl-BS>
+				update_n_lines = "ss",
+				suffix_last = "<Left>",
+				suffix_next = "<Right>",
+			},
+			search_method = "cover",
+			silent = true,
+		},
+	},
+	{
+		"echasnovski/mini.ai",
+		opts = {
+			mappings = {
+				around_next = "<S-Right>",
+				inside_next = "<Right>",
+				around_last = "<S-Left>",
+				inside_last = "<Left>",
+			},
+			search_method = "cover",
+			silent = true,
+		},
+	},
+	{ "echasnovski/mini.pairs", opts = {} },
+	{
+		"echasnovski/mini.files",
+		opts = {
+			windows = {
+				preview = true,
+				width_focus = 25,
+				width_nofocus = 25,
+				width_preview = 25,
 			},
 		},
 	},
+	{ "echasnovski/mini-git", main = "mini.git", opts = {} },
 }
