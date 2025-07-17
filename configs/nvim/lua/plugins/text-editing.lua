8555e4e44a367b3540213e78ab33916aaabb491d
Iori Pimentel <ioripimentel64@gmail.com>
2025-07-17 10:49:24 +0800

    nvim: Remove unneeded opts for treesj

--- configs/nvim/lua/plugins/text-editing.lua
+++ configs/nvim/lua/plugins/text-editing.lua
@@ -67,10 +67,6 @@ return {
 	{
 		"Wansmer/treesj",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		opts = { use_default_keymaps = false },
-		-- stylua: ignore
-		keys = {
-			{ "<leader>m", function() require("treesj").toggle() end }
-		},
+		opts = {},
 	},
 }
