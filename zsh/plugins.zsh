c4a510f01ce76dcf0cd7ae4bf5f91a43ac6c7639
Iori Pimentel <ioripimentel64@gmail.com>
2025-07-23 14:06:24 +0800

    zsh: anditote update changed format for plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,11 +1,11 @@
-fpath+=( $HOME/.cache/antidote/romkatv/powerlevel10k )
-source $HOME/.cache/antidote/romkatv/powerlevel10k/powerlevel10k.zsh-theme
-source $HOME/.cache/antidote/romkatv/powerlevel10k/powerlevel9k.zsh-theme
-fpath+=( $HOME/.cache/antidote/agkozak/zsh-z )
-source $HOME/.cache/antidote/agkozak/zsh-z/zsh-z.plugin.zsh
-fpath+=( $HOME/.cache/antidote/Aloxaf/fzf-tab )
-source $HOME/.cache/antidote/Aloxaf/fzf-tab/fzf-tab.plugin.zsh
-fpath+=( $HOME/.cache/antidote/zsh-users/zsh-autosuggestions )
-source $HOME/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( $HOME/.cache/antidote/zsh-users/zsh-syntax-highlighting )
-source $HOME/.cache/antidote/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( "$HOME/.cache/antidote/romkatv/powerlevel10k" )
+source "$HOME/.cache/antidote/romkatv/powerlevel10k/powerlevel10k.zsh-theme"
+source "$HOME/.cache/antidote/romkatv/powerlevel10k/powerlevel9k.zsh-theme"
+fpath+=( "$HOME/.cache/antidote/agkozak/zsh-z" )
+source "$HOME/.cache/antidote/agkozak/zsh-z/zsh-z.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/Aloxaf/fzf-tab" )
+source "$HOME/.cache/antidote/Aloxaf/fzf-tab/fzf-tab.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/zsh-users/zsh-autosuggestions" )
+source "$HOME/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/zsh-users/zsh-syntax-highlighting" )
+source "$HOME/.cache/antidote/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
